846f4625c1ea08065fc364ffb9a4b4eebb9149ae
denisbellin <45967516+denisbellin@users.noreply.github.com>
2020-01-28 15:52:53 +0100

    menu

--- socialbus/prova/lib/pages/login_register.dart
+++ socialbus/prova/lib/pages/login_register.dart
@@ -21,7 +21,7 @@ class _RegistrationState extends State<Registration> {
 
     Navigator.pushReplacement(context, MaterialPageRoute(
                                   builder: (context) =>
-                                      Home(user:user)), );
+                                      Home(user:user)));
   }
 
   @override
--- socialbus/prova/lib/widget/menu.dart
+++ socialbus/prova/lib/widget/menu.dart
@@ -6,7 +6,15 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String url = 'http://actv.avmspa.it/it/content/rete-venezia-unica-0';
+    String urlTime = 'http://actv.avmspa.it/it/content/rete-venezia-unica-0';
+    String urlHome = '/Users/denisbellin/codice/Ritardi-bus/socialbus/prova/lib/pages/home.dart';
+    String urlMoney = 'http://actv.avmspa.it/it/content/prezzi-vigore-0';
+    String urlWrite = '';
+    String urlRead = 'http://actv.avmspa.it/it/content/rete-venezia-unica-0';
+    String urlCom = 'http://actv.avmspa.it/it/content/rete-venezia-unica-0';
+    String urlSito = 'http://actv.avmspa.it/it/content/rete-venezia-unica-0';
+    
+
     return Drawer(
       child: ListView(
         children: <Widget>[
@@ -26,10 +34,110 @@ class Menu extends StatelessWidget {
           ),
           GestureDetector(
               onTap: () async {
-                if (await canLaunch(url)) {
-                  await launch(url);
+                if (await canLaunch(urlTime)) {
+                  await launch(urlTime);
                 } else {
-                  throw 'Could not launch $url';
+                  throw 'Could not launch $urlTime';
+                }
+              },
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.accessible_forward),
+                      Text(
+                        'ORARI',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              GestureDetector(
+              onTap: () async {
+                if (await canLaunch(urlMoney)) {
+                  await launch(urlMoney);
+                } else {
+                  throw 'Could not launch $urlMoney';
+                }
+              },
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.money_off),
+                      Text(
+                        'COSTI',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              GestureDetector(
+              onTap: () async {
+                if (await canLaunch(urlHome)) {
+                  await launch(urlHome);
+                } else {
+                  throw 'Could not launch $urlHome';
+                }
+              },
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.text_fields),
+                      Text(
+                        'HOME',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              GestureDetector(
+              onTap: () async {
+                if (await canLaunch(urlWrite)) {
+                  await launch(urlWrite);
+                } else {
+                  throw 'Could not launch $urlWrite';
+                }
+              },
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.local_post_office),
+                      Text(
+                        'SCRIVI',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              GestureDetector(
+              onTap: () async {
+                if (await canLaunch(urlRead)) {
+                  await launch(urlRead);
+                } else {
+                  throw 'Could not launch $urlRead';
                 }
               },
               child: Container(
@@ -39,7 +147,57 @@ class Menu extends StatelessWidget {
                     children: <Widget>[
                       Icon(Icons.attach_money),
                       Text(
-                        'BIGLIETTI & COSTI',
+                        'LEGGI',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              GestureDetector(
+              onTap: () async {
+                if (await canLaunch(urlCom)) {
+                  await launch(urlCom);
+                } else {
+                  throw 'Could not launch $urlCom';
+                }
+              },
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      Text(
+                        'COMUNICAZIONI',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              GestureDetector(
+              onTap: () async {
+                if (await canLaunch(urlSito)) {
+                  await launch(urlSito);
+                } else {
+                  throw 'Could not launch $urlSito';
+                }
+              },
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      Text(
+                        'RIFERIMENTI AL SITO',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: Colors.blue[800],
@@ -49,6 +207,7 @@ class Menu extends StatelessWidget {
                   ),
                 ),
               ))
+              
         ],
       ),
     );
