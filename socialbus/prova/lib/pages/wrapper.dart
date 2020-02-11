3d0b526c71b6d03b30a166644c8b9364501b4f8a
denisbellin <45967516+denisbellin@users.noreply.github.com>
2020-02-11 16:51:22 +0100

    aggiornamento grafica log in

--- socialbus/prova/lib/pages/wrapper.dart
+++ socialbus/prova/lib/pages/wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prova/service/graphicFn.dart';
 import 'login_register.dart';
 
 class Wrapper extends StatelessWidget {
@@ -11,70 +12,54 @@ class Wrapper extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.white,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: <Widget>[
-            Row(
+            Container(
+                width: MediaQuery.of(context).size.width * 1.00,
+                height: MediaQuery.of(context).size.height * 0.80,
+                decoration: BoxDecoration(color: hexToColor("#0058A5"))),
+            Container(
+              height: 300,
+              margin: EdgeInsets.all(100.0),
+              decoration:
+                  BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle
+                  ),
+            ),
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    // Stroked text as border.
-                    /*                   Text(
-                      'SocialBus',
-                      style: TextStyle(
-                      
-                        letterSpacing: 5,
-                        fontSize: 40,
-                        foreground: Paint()
-                          ..style = PaintingStyle.stroke
-                          ..strokeWidth = 7
-                          ..color = Colors.black,
-                      ),
-                    ),*/
-                    // Solid text as fill.
-                    Hero(
-                      tag: "title",
-                      child: Text(
-                        'SocialBus',
-                        style: TextStyle(
-                          //fontWeight: FontWeight.bold,
-                          letterSpacing: 5,
-                          fontSize: 40,
-                          color: Colors.grey,
+                SizedBox(
+                  height: MediaQuery.of(context).size.width * 0.10,
+                ),
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.50,
+                    child: Hero(
+                        tag: 'logo',
+                        child: Image.asset("assets/image/logo.png")
                         ),
-                      ),
-                    ),
-                  ],
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.width * 0.25,
+                ),
+                CustomButton(
+                  text: "Log In",
+                  callback: () {
+                    Navigator.of(context).pushNamed('/login');
+                  },
+                ),
+                CustomButton(
+                  text: "Register",
+                  callback: () {
+                    Navigator.of(context).pushNamed('/registration');
+                  },
                 )
               ],
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.10,
-            ),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.50,
-                child: Hero(
-                    tag: 'logo', child: Image.asset("assets/image/logo.png")),
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.25,
-            ),
-            CustomButton(
-              text: "Log In",
-              callback: () {
-                Navigator.of(context).pushNamed('/login');
-              },
-            ),
-            CustomButton(
-              text: "Register",
-              callback: () {
-                Navigator.of(context).pushNamed('/registration');
-              },
-            )
           ],
         ),
       ),
