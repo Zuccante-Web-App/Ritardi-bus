1edd2172f997c89017e66bd480addbb40b8c430d
Mattia Perpigano <mattyperpignano@gmail.com>
2020-02-18 14:45:35 +0100

    grafica wrapper

--- socialbus/prova/lib/pages/wrapper.dart
+++ socialbus/prova/lib/pages/wrapper.dart
@@ -15,92 +15,58 @@ class Wrapper extends StatelessWidget {
         child: Stack(
           children: <Widget>[
             Container(
-                width: MediaQuery.of(context).size.width * 1.00,
-                height: MediaQuery.of(context).size.height * 0.80,
+                height: MediaQuery.of(context).size.height * 0.65,
                 decoration: BoxDecoration(color: hexToColor("#0058A5"))),
             Container(
-              height: 300,
-              margin: EdgeInsets.all(100.0),
+              height: 375,
+              margin: EdgeInsets.all(90.0),
               decoration:
-                  BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle
-                  ),
+                  BoxDecoration(color: Colors.white, shape: BoxShape.circle),
             ),
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-<<<<<<< HEAD
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.17,
+                ),
                 Text(
                   'SocialBus',
                   style: TextStyle(
-                    //fontWeight: FontWeight.bold,
                     letterSpacing: 5,
                     fontSize: 40,
                     color: Colors.grey,
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.10,
-            ),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.50,
-                child: Hero(
-                    tag: 'logo', child: Image.asset("assets/image/logo.png")),
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.25,
-            ),
-            Text("Hai gia un accaunt:"),
-            CustomButton(
-              text: "Log In",
-              callback: () {
-                Navigator.of(context).pushNamed('/login');
-              },
-            ),
-            Text("Nuovo utente:"),
-            CustomButton(
-              text: "Registrati",
-              callback: () {
-                Navigator.of(context).pushNamed('/registration');
-              },
-            )
-=======
+                ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.width * 0.10,
+                  height: MediaQuery.of(context).size.height * 0.05,
                 ),
                 Center(
                   child: Container(
-                    width: MediaQuery.of(context).size.width * 0.50,
+                    width: MediaQuery.of(context).size.width * 0.46,
                     child: Hero(
                         tag: 'logo',
-                        child: Image.asset("assets/image/logo.png")
-                        ),
+                        child: Image.asset("assets/image/logo.png")),
                   ),
                 ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.width * 0.25,
+                  height: MediaQuery.of(context).size.height * 0.25,
                 ),
+                Text("Hai gia un accaunt:"),
                 CustomButton(
                   text: "Log In",
                   callback: () {
                     Navigator.of(context).pushNamed('/login');
                   },
                 ),
+                Text("Nuovo utente:"),
                 CustomButton(
-                  text: "Register",
+                  text: "Registrati",
                   callback: () {
                     Navigator.of(context).pushNamed('/registration');
                   },
-                )
+                ),
               ],
             ),
->>>>>>> 3d0b526c71b6d03b30a166644c8b9364501b4f8a
           ],
         ),
       ),
