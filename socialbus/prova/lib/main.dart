521e7f994028c802a5648bece453d9429678591b
TheXp <mattyperpignano@gmail.com>
2020-09-22 15:59:21 +0100

    social bus

--- socialbus/prova/lib/main.dart
+++ socialbus/prova/lib/main.dart
@@ -1,14 +1,14 @@
+//Social bus
 import 'package:flutter/material.dart';
 import 'package:prova/pages/home.dart';
 import 'package:prova/pages/wrapper.dart';
 import 'package:prova/pages/login_register.dart';
 
 void main() => runApp(MaterialApp(
-  title: 'Custom Fonts',
+      title: 'Custom Fonts',
       // Set Raleway as the default app font.
       theme: ThemeData(
         fontFamily: 'Comfortaa',
-        
       ),
       initialRoute: '/',
       routes: {
